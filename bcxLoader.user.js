49d4a402107ba31b06e12ce98d4f75d4df4256f5
shiroptr95 <151960901+shiroptr95@users.noreply.github.com>
2023-11-24 23:14:34 +0300

    Update bcxLoader.user.js

--- bcxLoader.user.js
+++ bcxLoader.user.js
@@ -3,7 +3,7 @@
 // @namespace    BCX
 // @version      1.0.5
 // @description  Loader of Jomshir's "Bondage Club Extended" mod RU
-// @author       Jomshir98
+// @author       Original Jomshir98 (fork shiroptr95)
 // @include      /^https:\/\/(www\.)?bondageprojects\.elementfx\.com\/R\d+\/(BondageClub|\d+)(\/((index|\d+)\.html)?)?$/
 // @include      /^https:\/\/(www\.)?bondage-europe\.com\/R\d+\/(BondageClub|\d+)(\/((index|\d+)\.html)?)?$/
 // @homepage     https://github.com/shiroptr95/bondage-club-extended#readme
